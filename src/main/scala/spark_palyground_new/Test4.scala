a6d24b3f743f5d01f104b68e93e4a643d8969d36
Kenche.vamshikrishna <krishna.vamshi878@gmail.com>
2019-11-22 21:15:54 +0530

    first commit-edited-4

---
+++ src/main/scala/spark_palyground_new/Test4.scala
@@ -0,0 +1,44 @@
+package spark_palyground_new
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Column
+
+object Test4 {
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger( "org" ).setLevel( Level.ERROR )
+
+    val spark = SparkSession.builder().
+      master( "local" ).appName( "sample" ).
+      getOrCreate()
+
+    import spark.implicits._
+
+    val df = Seq((1, "Saikrishna", "Hyderabad", "BE", 24), (2, "Aravind", "Mahabubnagar", "BAMS", 25),
+      (3, "Prakash", "Kotakonda", "B.Tech", 26)).toDF("id", "name", "loc", "qual", "age")
+    df.show
+
+    var df2 = df.select(struct('id, 'name) as "names", struct('loc, 'qual, 'age) as "info")
+    df2.show
+
+    val arr = df2.schema.names
+
+    arr.foreach {x =>
+      val nestCols = df2.schema(x).dataType.asInstanceOf[StructType].fields.map(x => x.name)
+      for (fds <- nestCols) {
+        df2 = df2.withColumn(fds, col(s"${x}.${fds}"))
+      }
+    }
+
+    df2.show
+
+    val df3 = df2.drop(arr:_*)
+
+    df3.show
+
+  }
+
+}
