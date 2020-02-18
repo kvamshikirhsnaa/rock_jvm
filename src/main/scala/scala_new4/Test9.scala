84312b1ea8fa2c126d4ee5243e01db4e898cfa8c
Kenche.vamshikrishna <krishna.vamshi878@gmail.com>
2020-02-18 14:30:39 +0530

    added udemy_rockjvm

--- src/main/scala/scala_new4/Test9.scala
+++ src/main/scala/scala_new4/Test9.scala
@@ -1,6 +1,7 @@
 package scala_new4
 
 import scala.annotation.tailrec
+import scala.util.Random
 
 object Test9 extends App {
 
@@ -30,6 +31,18 @@ object Test9 extends App {
     decomposePrimeTailrec(n,2, List.empty)
   }
 
+  def approximatePi(n: Int): Double = {
+    val random = new Random(System.currentTimeMillis())
+    val randomPtsInCircle = (1 to n).map{ _ =>
+      val x = random.nextDouble()
+      val y = random.nextDouble()
+
+      x * x + y * y
+    }.count(distance => distance < 1)
+
+    randomPtsInCircle * 4.0 / n
+  }
+
 
 
   println(getPrimes(50))  // List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
@@ -38,5 +51,17 @@ object Test9 extends App {
   println(decomposePrime(547))  // List(547)
   println(decomposePrime(2003))  // List(2003)
   println(decomposePrime(2002))  // List(13, 11, 7, 2)
+
+
+  println(Math.PI)   // 3.141592653589793
+  println(approximatePi(10))  // 3.6
+  println(approximatePi(100))  // 3.04
+  println(approximatePi(1000))  // 3.164
+  println(approximatePi(10000))  // 3.1836
+  println(approximatePi(100000))  // 3.14428
+  println(approximatePi(1000000)) // 3.142524
+  println(approximatePi(10000000)) // 3.1407944
+
+
 
 }
