1e7cf74a878463fc6a0a1321423407725763baed
Kenche.vamshikrishna <krishna.vamshi878@gmail.com>
2020-02-18 16:11:29 +0530

    added udemy_rockjvm

--- src/main/scala/scala_new4/Test9.scala
+++ src/main/scala/scala_new4/Test9.scala
@@ -33,20 +33,22 @@ object Test9 extends App {
 
   def approximatePi(n: Int): Double = {
     val random = new Random(System.currentTimeMillis())
-    val randomPtsInCircle = (1 to n).map{ _ =>
+    val randomPtsInSquare: List[Double] = (1 to n).toList.map{ _ =>
       val x = random.nextDouble()
       val y = random.nextDouble()
-
       x * x + y * y
-    }.count(distance => distance < 1)
-
+    }
+    val randomPtsInCircle = randomPtsInSquare.count(distance => distance < 1)
     randomPtsInCircle * 4.0 / n
   }
 
 
 
   println(getPrimes(50))  // List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+
+
   println(decomposePrime(16))  // List(2, 2, 2, 2)
+  println(decomposePrime(24)) // List(3, 2, 2, 2)
   println(decomposePrime(121))  // List(11, 11)
   println(decomposePrime(547))  // List(547)
   println(decomposePrime(2003))  // List(2003)
@@ -60,7 +62,8 @@ object Test9 extends App {
   println(approximatePi(10000))  // 3.1836
   println(approximatePi(100000))  // 3.14428
   println(approximatePi(1000000)) // 3.142524
-  println(approximatePi(10000000)) // 3.1407944
+  println(approximatePi(10000000)) // 3.1417344
+
 
 
 
